68e9e956acdb388540e3768a7040b67a4bf1241a
estra <estradamar2511@gmail.com>
2026-09-16 12:21:55 -0600

    first comit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/chassis.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp(name = "chasis", group = "Linear OpMode")
+public class chassis extends LinearOpMode {
+    private DcMotorEx frontLeft = null;
+    private DcMotorEx backLeft = null;
+    private DcMotorEx frontRight = null;
+    private DcMotorEx backRight = null;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "front_left");
+        backLeft = hardwareMap.get(DcMotorEx.class, "back_left");
+        frontRight = hardwareMap.get(DcMotorEx.class, "front_right");
+        backRight = hardwareMap.get(DcMotorEx.class, "back_right");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Get started", "Ready to begin");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x; // Se mueve lateral
+            double rx = gamepad1.right_stick_x; // Gira
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 2.0);
+
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+
+            telemetry.addData("Front left/right", "%.2f / %.2f", frontLeftPower, frontRightPower);
+            telemetry.addData("Back left/right", "%.2f / %.2f", backLeftPower, backRightPower);
+            telemetry.update();
+        }
+    }
+}
